5ddd7fefe18ea6a17ebbe3169c4376a1db2a2070
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2022-06-25 20:50:58 +0300

    feat(movement-mechanics): Movement mechanics is developed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,25 @@
+using System;
 using UnityEngine;
 using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 20f;
+    [SerializeField] private float speed = 1f;
     [SerializeField] private Transform playerStacks;
     [SerializeField] private float altitudeMultiplier = 0.075f;
     private Animator animator;
+    private BoxCollider boxCollider;
     private bool canMove = true;
     Vector3 directionVector = Vector3.zero;
-    private float currentAltitude = 0f;
-    private int playerStacksCounter = 0;
+    private float currentAltitude;
+    private int playerStacksCounter = 1;
     
     void Start()
     {
         EventManager.current.onSwipe += OnSwipe;
         animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
         currentAltitude = transform.position.y;
-    }
-
-    private void Update()
-    {
-        // if (canMove)
-        // {
-        //     transform.Translate(directionVector * speed * Time.deltaTime, Space.World);
-        // }
     }
 
     void OnSwipe(string direction)
@@ -45,20 +40,74 @@ public class PlayerController : MonoBehaviour
                 directionVector = Vector3.back;
                 break;
         }
-        HandleMovement(directionVector);
+        
+        bool isThereRoad = SendRaycastToFindRoad(directionVector);
+        if (isThereRoad)
+        {
+            HandleMovement();
+        }
     }
     
-    void HandleMovement(Vector3 directionVector)
+    void HandleMovement()
     {
-        LookRoad(directionVector);
-        canMove = true;
+        AdjustTheColliderDirection();
+        StartMovement();
+    }
+
+    void StartMovement()
+    {
+        if (canMove)
+        {
+            canMove = false;
+            if (directionVector.x != 0)
+                transform.DOMoveX(directionVector.x * 100, 100/speed).SetId("Movement").SetUpdate(UpdateType.Fixed);
+            else if (directionVector.z != 0)
+                transform.DOMoveZ(directionVector.z * 100, 100/speed).SetId("Movement").SetUpdate(UpdateType.Fixed);
+        }
     }
     
-    void LookRoad(Vector3 directionVector)
+    void StopMovement()
     {
-        //Player on the ground?
-        Vector3 origin = transform.position + Vector3.up + directionVector;
-        Vector3 direction = -transform.up;
+        DOTween.Kill("Movement");
+        Vector3 stopVector = new Vector3((float)Math.Round(transform.position.x), transform.position.y, (float)Math.Round(transform.position.z));
+        transform.DOMove(stopVector, 0.5f).OnComplete(()=>canMove = true);
+    }
+    
+    void AdjustTheColliderDirection()
+    {
+        Vector3 boxColliderLocationVector = Vector3.zero;
+        Vector3 boxColliderSizeVector = new Vector3(0.1f, 0.1f, 0.1f);
+        float location = 0.25f, size = 1f;
+        boxColliderLocationVector.y = location;
+        if (directionVector.x > 0)
+        {
+            boxColliderLocationVector.x = location;
+            boxColliderSizeVector.x = size;
+        }
+        else if (directionVector.x < 0)
+        {
+            boxColliderLocationVector.x = -location;
+            boxColliderSizeVector.x = size;
+        }
+        else if (directionVector.z > 0)
+        {
+            boxColliderLocationVector.z = location;
+            boxColliderSizeVector.z = size;
+        }
+        else if (directionVector.z < 0)
+        {
+            boxColliderLocationVector.z = -location;
+            boxColliderSizeVector.z = size;
+        }
+
+        boxCollider.center = boxColliderLocationVector;
+        boxCollider.size = boxColliderSizeVector;
+    }
+    
+    bool SendRaycastToFindRoad(Vector3 directionVector)
+    {
+        Vector3 origin = transform.position + Vector3.up + directionVector; //From the player's body
+        Vector3 direction = Vector3.down; // Downwards
         bool isThereRoad = false;
         RaycastHit hit;
         
@@ -66,27 +115,13 @@ public class PlayerController : MonoBehaviour
         if (Physics.Raycast(origin, direction, out hit,500))
         {
             GameObject obj = hit.transform.gameObject;
-            if (obj.tag.Equals("Road") || obj.tag.Equals("Stack") || obj.tag.Equals("Gate"))
+            if (obj.tag.Equals("Road") || obj.tag.Equals("Stack"))
             {
                 isThereRoad = true;
             }
         }
-        else
-        {
-            isThereRoad = false;
-        }
-        
-        Debug.Log(isThereRoad);
-            
 
-    }
-    
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag.Equals("Border"))
-        {
-            canMove = false;
-        }
+        return isThereRoad;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -95,16 +130,22 @@ public class PlayerController : MonoBehaviour
         {
             currentAltitude += altitudeMultiplier;
             playerStacksCounter += 1;
-            transform.DOKill(false);
-            transform.DOMoveY(currentAltitude, 0.2f).SetRelative(false);
-            animator.SetTrigger("Jump");
-
+            DOTween.Kill("Rising");
+            transform.DOMoveY(currentAltitude, 0.2f).SetRelative(false).SetId("Rising");
+            animator.SetBool("Jump", true);
+            
             Transform stack = other.transform;
             stack.gameObject.GetComponent<BoxCollider>().enabled = false;
             stack.parent = playerStacks;
             stack.localEulerAngles = Vector3.zero;
             stack.transform.localPosition = Vector3.zero;
             stack.transform.localPosition = new Vector3(stack.localPosition.x, (playerStacksCounter - 1) * -altitudeMultiplier ,stack.localPosition.z);
+        }
+        
+        if (other.gameObject.tag.Equals("Gate"))
+        {
+            animator.SetBool("Jump", false);
+            StopMovement();
         }
     }
 
